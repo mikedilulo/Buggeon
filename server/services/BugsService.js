f30ca4a095fc3e38d1a6fd22a840a3029b7b7d21
mikedilulo <mikedilulo2019@gmail.com>
2019-12-27 14:47:19 -0700

    Bugs Service Methods Completed

--- server/services/BugsService.js
+++ server/services/BugsService.js
@@ -1,11 +1,41 @@
 import mongoose from "mongoose";
 import Bug from "../models/Bug";
+import ApiError from "../utils/ApiError";
 
 const _repository = mongoose.model("Bug", Bug);
 
 class BugsService {
   async getAll() {
     return await _repository.find({});
+  }
+  async getBugById(id) {
+    let data = await _repository.findById(id);
+    if (!data) {
+      throw new ApiError("Invalid Bug Id", 400);
+    }
+    return data;
+  }
+  async createBug(rawData) {
+    let data = await _repository.create(rawData);
+    return data;
+  }
+  async editBug(id, update) {
+    let data = await _repository.findByIdAndUpdate({ _id: id }, update, {
+      new: true
+    });
+    if (!data) {
+      throw new ApiError("Invalid Bug ID To Update", 400);
+    }
+    return data;
+  }
+  async deleteBug(id) {
+    let data = await _repository.findOneAndUpdate(
+      { _id: id },
+      { closed: true }
+    );
+    if (!data) {
+      throw new ApiError("Invalid Bug ID to Delete", 400);
+    }
   }
 }
 
