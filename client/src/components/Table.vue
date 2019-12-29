05cec355e5d71396e72de939e84fb327dae2f687
mikedilulo <mikedilulo2019@gmail.com>
2019-12-29 13:35:03 -0700

    Added Hide Closed Bugs Button inside Div

--- client/src/components/Table.vue
+++ client/src/components/Table.vue
@@ -2,27 +2,31 @@
   <div class="table">
     <div class="row container-fluid d-flex justify-content-center mt-5">
       <div class="col-md-10">
-        <table class="table table-hover">
-          <thead class="thead-dark">
-            <tr>
-              <th scope="col">Bug Title</th>
-              <th scope="col">Reported By</th>
-              <th scope="col">Status of Bug</th>
-              <th scope="col">Bug Created</th>
-              <th scope="col">Last Modified</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr class="table" v-for="bug in bug" :key="bug.id">
-              <td>{{bug.title}}</td>
-              <td>{{bug.reportedBy}}</td>
-              <td>{{bug.closed}}</td>
-              <td>{{bug.createdAt}}</td>
-              <td>{{bug.updatedAt}}</td>
-              <table :bugData="bug" />
-            </tr>
-          </tbody>
-        </table>
+        <h1 class="mb-5">**Reported Bugs**</h1>
+        <div>
+          <button class="btn btn-warning text-dark btn-lg mb-4 closeButton">Hide Closed Bugs</button>
+          <table class="table table-hover">
+            <thead class="thead-dark">
+              <tr>
+                <th scope="col">Bug Title</th>
+                <th scope="col">Reported By</th>
+                <th scope="col">Status of Bug</th>
+                <th scope="col">Bug Created</th>
+                <th scope="col">Last Modified</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="table" v-for="bug in bug" :key="bug.id">
+                <td>{{bug.title}}</td>
+                <td>{{bug.reportedBy}}</td>
+                <td>{{bug.closed}}</td>
+                <td>{{bug.createdAt}}</td>
+                <td>{{bug.updatedAt}}</td>
+                <table :bugData="bug" />
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   </div>
@@ -47,4 +51,7 @@ export default {
 </script>
 
 <style>
+.closeButton {
+  margin-left: 1335px !important;
+}
 </style>
